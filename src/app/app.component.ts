7e3a73ff8af3f1c954a47533dc2625629ca10169
Scott Potter <scott.g.potter@gmail.com>
2017-04-21 13:53:38 -0400

    services working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+
+
+
+
+export class AppComponent implements OnInit {
+  title: 'my hereos';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log('selected ' + hero.name);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Scott on 4/21/2017.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
