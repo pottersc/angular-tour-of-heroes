bf886a5d97ab69f9d4e82ff72686d44af2e7f735
Scott Potter <scott.g.potter@gmail.com>
2017-04-23 09:14:51 -0400

    services working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
-
+import {Component} from '@angular/core';
+/**
+ * Created by Scott on 4/21/2017.
+ */
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
 
-
-
-
-
-export class AppComponent implements OnInit {
-  title: 'my hereos';
-  selectedHero: Hero;
-  heroes: Hero[];
-
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    console.log('selected ' + hero.name);
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
+export class AppComponent {
+  title: 'Tour of Heroes';
 }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
   @Input() hero: Hero;
+  // hero: Hero;
 }
