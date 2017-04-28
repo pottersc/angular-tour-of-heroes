5fe5f38a0d049f6c536d4e0c8b8cad770e845a1f
esp003 <scott.potter@motorolasolutions.com>
2017-04-28 16:56:13 -0400

    play with observables

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+const dragonEventsConst = [
+  { type: 'attack', value: 12, target: 'player-dorkman'},
+  { type: 'yawn', value: 40, target: 'none'},
+  { type: 'eat',  value: -1, target: 'horse'},
+  { type: 'attack', value: 23, target: 'player-scott'},
+  { type: 'attack', value: 12, target: 'player-dorkman'}
+];
+
+const dragonResult = dragonEventsConst
+  .filter(event => event.type === 'attack')
+  .filter(event => event.target === 'player-dorkman')
+  .map(event => event.value)
+  .reduce((prev, value) => (prev || 0) + value);
 
 
 @Component({
@@ -6,7 +21,54 @@ import { Component } from '@angular/core';
   templateUrl: './test.component.html'
 })
 
-export class TestComponent {
+
+export class TestComponent  implements OnInit {
+  dragonEvents = dragonEventsConst;
+  title = '';
+  result = JSON.stringify(dragonResult);
+ // source = undefined;
+
+  ngOnInit(): void {
+    this.dragonEvents = dragonEventsConst;
+    this.title = 'My Title';
+
+    const source = Observable.create((observer) => {
+      setTimeout(() => {
+        observer.next('observable timeout message');
+      }, 2000);
+      console.log('observable started');
+    });
+
+   // this.source.forEach(x => console.log(x));
+    source.subscribe(value => console.log(value));
+  }
+
+  getDragonEventsStr() {
+    return JSON.stringify(this.dragonEvents);
+  }
+
+  testClick() {
+    console.log('Clicked');
+  }
+
+  onkey(value: string) {
+    console.log('keyup:' + value);
+}
 
 
+  testInput() {
+    console.log('value');
+  }
+
+
+
+  /*
+  testInput(value) {
+    console.log('value=' + value);
+  }
+
+  testInput(value: string): void {
+    console.log('value=' + value);
+  }
+*/
 }
